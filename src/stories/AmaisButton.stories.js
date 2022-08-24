a3b9848b37766771341ae9c11ec47210c53456fd
Pedro Gonçalves | amais <pedro.goncalves@amaiseducacao.com.br>
2022-08-24 18:41:51 -0300

    created AmaisButton.stories.js

--- src/stories/AmaisButton.stories.js
+++ src/stories/AmaisButton.stories.js
@@ -1,27 +1,28 @@
 import AmaisButton from "../../src/components/AmaisButton.vue";
 
 export default {
-    title: "Buttonasd",
+    title: "Amais/Button",
     component: AmaisButton,
 };
 
 const Template = args => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { AmaisButton },
-    // The story's `args` need to be mapped into the template through the `setup()` method
+
     setup() {
         return { args };
     },
-    // And then the `args` are bound to your component with `v-bind="args"`
+
     template: `
         <AmaisButton v-bind="args">
-            teste
+            <template v-if="${"defaultSlot" in args}" v-slot>
+                ${args.defaultSlot}
+            </template>
         </AmaisButton>
     `,
 });
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
     primary: true,
+    defaultSlot: "<b>aaaaaa</b>",
 };
