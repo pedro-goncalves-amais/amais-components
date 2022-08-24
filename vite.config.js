54df97f7047f040408772df07d86c52edfdc8a9d
Pedro Gonçalves | amais <pedro.goncalves@amaiseducacao.com.br>
2022-08-24 16:53:28 -0300

    remove vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +24,5 @@ export default defineConfig({
             },
         },
     },
-    plugins: [eslint(), vue()],
+    plugins: [vue()],
 });
